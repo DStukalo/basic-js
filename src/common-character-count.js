a2e46f36f5b04e88c78183edae1a51e634859c8f
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-04-30 22:16:05 +0300

    added sum-digits

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+    let arr1 = [...s1];
+    let arr2 = [...s2];
+    let count1 = new Map();
+    arr1.forEach((el) => {
+        if (count1.has(el)) {
+            count1.el++;
+        }
+    });
+    return count1;
 }
 
 module.exports = {
-  getCommonCharacterCount
+    getCommonCharacterCount,
 };
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -19,29 +19,21 @@ function getSumOfDigits(n) {
     } else {
         str = n;
     }
-    let arr = str.split("");
     let arr2 = [];
-    let res;
-    arr.forEach((element) => {
+    let arr = str.split("").forEach((element) => {
         arr2.push(Number(element));
     });
-    res = arr2.reduce((acc, val) => acc + val, 0);
-    // arr3 = String(arr3);
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    // if (res.length) return res.length;
+    let res = arr2.reduce((acc, val) => acc + val, 0);
     if (res >= 10) {
         let str = String(res);
-        let ktArr = str.split("");
-        let ktArr2 = [];
-        ktArr.forEach((element) => {
-            ktArr2.push(Number(element));
+        let arr = str.split("");
+        let arr2 = [];
+        arr.forEach((element) => {
+            arr2.push(Number(element));
         });
-        res = ktArr2.reduce((acc, val) => acc + val, 0);
+        res = arr2.reduce((acc, val) => acc + val, 0);
     }
-    // getSumOfDigits(arr3);
     return res;
-    // return Number(arr3);
 }
 
 module.exports = {
