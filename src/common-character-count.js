a4b5cb7609d0c112364c9cb2fa8bee5a506a46a9
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-05-01 10:36:41 +0300

    added delete-digits

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,15 +12,35 @@ const { NotImplementedError } = require("../extensions/index.js");
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-    let arr1 = [...s1];
-    let arr2 = [...s2];
-    let count1 = new Map();
-    arr1.forEach((el) => {
-        if (count1.has(el)) {
-            count1.el++;
+    let count1 = 0;
+    let count2 = 0;
+    const symbols1 = new Map();
+    for (const key of s1) {
+        const currCount = symbols1.get(key) || 0;
+        symbols1.set(key, currCount + 1);
+    }
+    const orderedSymbols1 = [...symbols1.values()];
+    orderedSymbols1.forEach((el) => {
+        if (el >= 2) {
+            count1 += Math.floor(el / 2);
         }
     });
-    return count1;
+
+    const symbols2 = new Map();
+    for (const key of s2) {
+        const currCount = symbols2.get(key) || 0;
+        symbols2.set(key, currCount + 1);
+    }
+    const orderedSymbols2 = [...symbols2.values()];
+    orderedSymbols2.forEach((el) => {
+        if (el >= 2) {
+            count2 += Math.floor(el / 2);
+        }
+    });
+    res = count1 + count2;
+    return res;
+    // throw new NotImplementedError("Not implemented");
+    // remove line with error and write your code here
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+    let arr = [];
+    let res = [];
+    let arrStr = String(n).split("");
+    for (let i = 0; i < arrStr.length; i++) {
+        let cur;
+        if (i === 0) {
+            cur = arrStr.slice(1);
+        } else if (i < arrStr.length - 1) {
+            cur = arrStr.slice(0, i).concat(arrStr.slice(i + 1, arrStr.length));
+        } else {
+            cur = arrStr.slice(0, arrStr.length - 1);
+        }
+        arr.push(cur);
+    }
+    arr.forEach((el) => {
+        let localres = "";
+        el.forEach((element) => {
+            localres += element;
+        });
+        localres = Number(localres);
+        res.push(localres);
+    });
+    res = Math.max(...res);
+    return res;
 }
 
 module.exports = {
-  deleteDigit
+    deleteDigit,
 };
